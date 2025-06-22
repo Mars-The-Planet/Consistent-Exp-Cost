4dc13001339949ccb58953fc38e0c2af7bb74235
Mars <ruzicka.maty@gmail.com>
2025-06-22 13:26:46 +0200

    fixed calculating set_level_cost

--- common/src/main/java/com/mars/consistentexpcost/mixin/AnvilMenuMixin.java
+++ common/src/main/java/com/mars/consistentexpcost/mixin/AnvilMenuMixin.java
@@ -15,7 +15,7 @@ public class AnvilMenuMixin {
     @Redirect(method = "onTake(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;giveExperienceLevels(I)V"))
     private void onTake(Player player, int levelCost) {
-        player.giveExperiencePoints(use_minimal_exp_cost ? -getTotalXpAtLevel(-levelCost) : -set_level_cost);
+        player.giveExperiencePoints(use_minimal_exp_cost ? -getTotalXpAtLevel(-levelCost) : levelCost * set_level_cost);
 
         if (player.experienceLevel < 0) {
             player.experienceLevel = 0;
