f77b2f80dbec259a187d2905980ec7d9434be87e
Mars-The-Planet <microsoft.maty@gmail.com>
2026-08-21 20:00:05 +0200

    fixed set level costs

---
+++ common/src/main/java/com/mars/consistentexpcost/mixin/EnchantmentMenuMixin.java
@@ -0,0 +1,34 @@
+package com.mars.consistentexpcost.mixin;
+
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+import static com.mars.consistentexpcost.CommonClass.getMinimumLevelForXp;
+import static com.mars.consistentexpcost.ConfigOptions.set_level_cost;
+import static com.mars.consistentexpcost.ConfigOptions.use_minimal_exp_cost;
+
+@Mixin(EnchantmentMenu.class)
+public abstract class EnchantmentMenuMixin {
+
+    @Shadow @Final
+    public int[] costs;
+
+    @Inject(at = @At("HEAD"), method = "getEnchantmentList")
+    public void getEnchantmentList(FeatureFlagSet enabledFeatures, ItemStack stack, int enchantSlot, int level, CallbackInfoReturnable<List<EnchantmentInstance>> cir) {
+        if (use_minimal_exp_cost) return;
+
+        int newCost = getMinimumLevelForXp(set_level_cost * (enchantSlot + 1));
+        if (newCost > costs[enchantSlot])
+            costs[enchantSlot] = newCost;
+    }
+}
