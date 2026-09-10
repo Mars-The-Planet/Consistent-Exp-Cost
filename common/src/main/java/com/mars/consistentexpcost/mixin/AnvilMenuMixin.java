2c35a4acf30e37c1d291850ccd29f65c9fe9ed82
Mars-The-Planet <microsoft.maty@gmail.com>
2026-09-10 15:47:39 +0200

    removed the cap of 40 levels on anvils

--- common/src/main/java/com/mars/consistentexpcost/mixin/AnvilMenuMixin.java
+++ common/src/main/java/com/mars/consistentexpcost/mixin/AnvilMenuMixin.java
@@ -3,26 +3,24 @@ package com.mars.consistentexpcost.mixin;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AnvilMenu;
-import net.minecraft.world.inventory.DataSlot;
+import net.minecraft.world.inventory.*;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.mars.consistentexpcost.CommonClass.getMinimumLevelForXp;
 import static com.mars.consistentexpcost.CommonClass.getTotalXpAtLevel;
-import static com.mars.consistentexpcost.ConfigOptions.set_level_cost;
-import static com.mars.consistentexpcost.ConfigOptions.use_minimal_exp_cost;
+import static com.mars.consistentexpcost.ConfigOptions.*;
 
 @Mixin(AnvilMenu.class)
-public class AnvilMenuMixin {
+public abstract class AnvilMenuMixin extends ItemCombinerMenu  {
 
     @Shadow
     @Final
@@ -30,6 +28,10 @@ public class AnvilMenuMixin {
 
     @Unique
     boolean consistentexpcost$trip = true;
+
+    public AnvilMenuMixin(@Nullable MenuType<?> menuType, int containerId, Inventory inventory, ContainerLevelAccess access, ItemCombinerMenuSlotDefinition itemInputSlots) {
+        super(menuType, containerId, inventory, access, itemInputSlots);
+    }
 
     @Redirect(method = "onTake(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;giveExperienceLevels(I)V"))
@@ -43,13 +45,22 @@ public class AnvilMenuMixin {
         }
     }
 
-    @Inject(at = @At("HEAD"), method = "createResult")
+    @Inject(at = @At("HEAD"), method = {"createResult", "createResultInternal"})
     public void onTake(CallbackInfo ci) {
         consistentexpcost$trip = true;
+
+        if (remove_too_expensive && this.cost.get() >= 40) {
+            if (use_minimal_exp_cost) {
+                this.player.sendSystemMessage(Component.literal("Ignore \"TOO EXPENSIVE\" you can pick the item. It will cost " + this.cost.get() + " levels.").withStyle(ChatFormatting.GREEN));
+            }
+            else {
+                this.player.sendSystemMessage(Component.literal("Ignore \"TOO EXPENSIVE\" you can pick the item. It will cost " + this.cost.get() * set_level_cost + " EXP.").withStyle(ChatFormatting.GREEN));
+            }
+        }
     }
 
     @Inject(at = @At("TAIL"), method = "mayPickup", cancellable = true)
-    public void mayPickup(Player player, boolean hasStack, CallbackInfoReturnable<Boolean> cir) {
+    public void mayPickup(Player player, boolean hasItem, CallbackInfoReturnable<Boolean> cir) {
         if (use_minimal_exp_cost) return;
 
         int newCost = set_level_cost * this.cost.get();
@@ -61,5 +72,12 @@ public class AnvilMenuMixin {
             }
             cir.setReturnValue(false);
         }
+    }
+
+    @ModifyConstant(method = {"createResult", "createResultInternal"}, constant = @Constant(intValue = 40))
+    private int whenTooExpensive(int value) {
+        if (!remove_too_expensive)
+            return 40;
+        return Integer.MAX_VALUE;
     }
 }
